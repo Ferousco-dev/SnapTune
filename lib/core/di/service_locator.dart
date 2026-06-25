a653884535312334f94f30284bf941e57a5e544f
Ferousco-dev <ferouslos6@gmail.com>
2026-06-26 00:29:35 +0100

    wire service_locator: register gallery datasource, repo, use cases, bloc

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,16 +1,33 @@
 import 'package:get_it/get_it.dart';
+import '../../features/gallery/data/datasources/gallery_local_datasource.dart';
+import '../../features/gallery/data/repositories/gallery_repository_impl.dart';
+import '../../features/gallery/domain/repositories/gallery_repository.dart';
+import '../../features/gallery/domain/usecases/get_recent_media.dart';
+import '../../features/gallery/domain/usecases/request_gallery_permission.dart';
+import '../../features/gallery/presentation/bloc/gallery_bloc.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> initDependencies() async {
   _registerGalleryDependencies();
-  _registerOptimizationDependencies();
 }
 
 void _registerGalleryDependencies() {
-  // Registered when gallery feature is built
-}
+  sl.registerLazySingleton<GalleryLocalDatasource>(
+    () => GalleryLocalDatasourceImpl(),
+  );
 
-void _registerOptimizationDependencies() {
-  // Registered when optimization feature is built
+  sl.registerLazySingleton<GalleryRepository>(
+    () => GalleryRepositoryImpl(sl()),
+  );
+
+  sl.registerLazySingleton(() => RequestGalleryPermission(sl()));
+  sl.registerLazySingleton(() => GetRecentMedia(sl()));
+
+  sl.registerFactory(
+    () => GalleryBloc(
+      requestPermission: sl(),
+      getRecentMedia: sl(),
+    ),
+  );
 }
