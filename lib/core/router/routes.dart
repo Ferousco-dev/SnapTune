0b407d6e33f2a0b62835d4e6c4c45d40b1122a6e
Ferousco-dev <ferouslos6@gmail.com>
2026-06-25 23:44:42 +0100

    feat(router): add route path constants

---
+++ lib/core/router/routes.dart
@@ -0,0 +1,6 @@
+abstract final class Routes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String gallery = '/gallery';
+  static const String viewer = '/viewer';
+}
